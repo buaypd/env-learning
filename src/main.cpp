2e57c0311c2c71a714166db8f5b8dc79d9173f21
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-01-08 17:43:36 +0000

    Made jumping game

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,44 @@
-#include <bn_core.h>
 #include <bn_backdrop.h>
-#include <bn_color.h>
+#include <bn_core.h>
 #include <bn_keypad.h>
+#include <bn_sprite_ptr.h>
+
+#include "bn_sprite_items_dot.h"
+
+#define FLOOR (80 - 4)
 
 int main() {
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(31, 0, 0));
+    bn::backdrop::set_color(bn::color(15, 0, 0));
+
+    auto dot = bn::sprite_items::dot.create_sprite(0, 0);
+
+    bn::fixed speed = 1.5;
+
+    bn::fixed dy = 0;
+    bn::fixed gravity = .03;
+
+    bn::fixed jump_strength = 1;
 
     while(true) {
-        if(bn::keypad::a_pressed()) {
-            bn::backdrop::set_color(bn::color(0, 0, 30));
+        if(bn::keypad::left_held()) {
+            dot.set_x(dot.x() - speed);
         }
-        if(bn::keypad::b_pressed()) {
-            bn::backdrop::set_color(bn::color(0, 31, 0));
+        if(bn::keypad::right_held()) {
+            dot.set_x(dot.x() + speed);
+        }
+        if(bn::keypad::a_pressed()) {
+            dy -= jump_strength;
+        }
+
+        dy += gravity;
+
+        dot.set_y(dot.y() + dy);
+
+        if(dot.y() > FLOOR) {
+            dot.set_y(FLOOR);
+            dy = 0;
         }
         bn::core::update();
     }
